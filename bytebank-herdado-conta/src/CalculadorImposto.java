e00d68382503a0ec564a9413f92ad4e3d81ad35e
eliane <eliane.oliveira.ext@synchro.com.br>
2023-02-16 15:08:54 -0300

    updating repository

---
+++ bytebank-herdado-conta/src/CalculadorImposto.java
@@ -0,0 +1,14 @@
+public class CalculadorImposto {
+
+    private double totalImposto;
+
+    public double getTotalImposto() {
+        return totalImposto;
+    }
+
+    public void registra(Tributavel t) {
+
+        double valor = t.getValorImposto();
+        this.totalImposto += valor;
+    }
+}
