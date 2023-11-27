483ccbb5c340ec0cbec3a95e5d6faeaea9fd9c38
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-11-27 17:24:31 -0300

    feat: criando DTOs

---
+++ boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/DTO/AprovacaoAdocaoDTO.java
@@ -0,0 +1,6 @@
+package br.com.alura.adopet.api.DTO;
+
+import jakarta.validation.constraints.NotNull;
+
+public record AprovacaoAdocaoDTO(@NotNull Long idAdocao) {
+}
---
+++ boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/DTO/SolicitacaoAdocaoDTO.java
@@ -0,0 +1,4 @@
+package br.com.alura.adopet.api.DTO;
+
+public record SolicitacaoAdocaoDTO(Long idPet, Long idTutor, String motivo) {
+}
