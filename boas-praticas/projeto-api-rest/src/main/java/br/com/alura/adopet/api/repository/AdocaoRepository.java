3561d832fb7110cf369372d823449ea09fe0f605
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-11-28 16:38:53 -0300

    refactor: otimizando consultas

--- boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/repository/AdocaoRepository.java
+++ boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/repository/AdocaoRepository.java
@@ -4,10 +4,14 @@ import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
 
     boolean existsByPetIdAndStatus(Long idPet, StatusAdocao status);
 
     boolean existsByTutorIdAndStatus(Long idTutor, StatusAdocao status);
+
+    int countAdocaoByTutorIdAndStatus(Long idTutor, StatusAdocao status);
 
 }
