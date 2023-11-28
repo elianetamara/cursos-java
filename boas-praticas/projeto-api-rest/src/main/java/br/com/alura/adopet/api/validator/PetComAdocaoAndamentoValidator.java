194df23fbcba5abc60aa145cb3e0e17c601d8d3b
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-11-28 14:27:11 -0300

    refactor: movendo validacoes utilizando padrao strategy

---
+++ boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/validator/PetComAdocaoAndamentoValidator.java
@@ -0,0 +1,33 @@
+package br.com.alura.adopet.api.validator;
+
+import br.com.alura.adopet.api.DTO.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PetComAdocaoAndamentoValidator{
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Autowired
+    private AdocaoRepository adocaoRepository;
+
+    public void validar(SolicitacaoAdocaoDTO dto) {
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+        List<Adocao> adocoes = adocaoRepository.findAll();
+        for (Adocao a : adocoes) {
+            if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
+            }
+        }
+    }
+}
---
+++ boas-praticas/projeto-api-rest/src/main/java/br/com/alura/adopet/api/validator/TutorComAdocaoAndamentoValidator.java
@@ -0,0 +1,33 @@
+package br.com.alura.adopet.api.validator;
+
+import br.com.alura.adopet.api.DTO.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TutorComAdocaoAndamentoValidator{
+
+    @Autowired
+    private AdocaoRepository adocaoRepository;
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    public void validar(SolicitacaoAdocaoDTO dto) {
+        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
+        List<Adocao> adocoes = adocaoRepository.findAll();
+        for (Adocao a : adocoes) {
+            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
+            }
+        }
+    }
+}
