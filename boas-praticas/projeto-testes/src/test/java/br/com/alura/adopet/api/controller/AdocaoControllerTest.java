159716a4fa4bdbfd9e82f20d71a152aa3deaa280
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-11-29 17:27:20 -0300

    test: AdocaoControllerTest

---
+++ boas-praticas/projeto-testes/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,73 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService adocaoService;
+
+    @Test
+    @DisplayName("Devolver código 400 para solicitação de adoção com erros")
+    void solicitaComErros() throws Exception {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Devolver código 200 para solicitação de adoção sem erros")
+    void solicitaSemErros() throws Exception {
+        //ARRANGE
+        String json = """
+            {
+                "idPet": 1,
+                "idTutor": 1,
+                "motivo": "Motivo qualquer"
+            }
+            """;
+
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    void aprovar() {
+    }
+
+    @Test
+    void reprovar() {
+    }
+}
