7648c62ac594c52bcdbd5ea5823087c40b3dbb51
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-11-29 15:15:43 -0300

    test: CalculadoraProbabilidadeAdocaoTest

---
+++ boas-praticas/projeto-testes/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,68 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    private CalculadoraProbabilidadeAdocao calculadora;
+
+    @BeforeEach
+    void setUp() {
+        calculadora = new CalculadoraProbabilidadeAdocao();
+    }
+
+    @Test
+    @DisplayName("Probabilidade alta para gatos jovens com peso baixo")
+    void calculaProbAlta() {
+        //idade 4 anos e 4kg - ALTA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        ), abrigo);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, calculadora.calcular(pet));
+    }
+
+    @Test
+    @DisplayName("Probabilidade média para gatos idosos com peso baixo")
+    void calculaProbMedia() {
+        //idade 15 anos e 4kg - MEDIA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                15,
+                "Cinza",
+                4.0f
+        ), abrigo);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.MEDIA, calculadora.calcular(pet));
+    }
+}
