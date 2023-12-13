16b0a749ef998f9fe04ba07802e7fb42db65d9ce
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-12-13 14:36:35 -0300

    raw: ajuste de package

--- java-spring-boot/boas-praticas-spring/src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ java-spring-boot/boas-praticas-spring/src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,11 +1,11 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
@@ -15,7 +15,8 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Paciente {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
