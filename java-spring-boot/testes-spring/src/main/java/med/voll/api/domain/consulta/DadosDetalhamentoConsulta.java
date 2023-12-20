d94ad3bd8b29e87678e41cf4568c47ad21562dbc
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-12-20 17:12:01 -0300

    feat: criacao de consultas

---
+++ java-spring-boot/testes-spring/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(Medico medico, @NotNull Paciente paciente, @NotNull @Future LocalDateTime data, Especialidade especialidade) {
+}
