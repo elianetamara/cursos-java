25d1ca3059f3371b9d5f70a83150daead8443054
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-12-21 16:43:05 -0300

    chore: agendamento de consultas

--- java-spring-boot/testes-spring/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ java-spring-boot/testes-spring/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -33,8 +33,10 @@ public class AgendaDeConsultas {
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
         if (dados.idMedico() != null)
-           return medicoRepository.findById(dados.idMedico()).get();
-        return null;
+           return medicoRepository.getReferenceById(dados.idMedico());
+        if (dados.especialidade() == null)
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!");
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
 }
